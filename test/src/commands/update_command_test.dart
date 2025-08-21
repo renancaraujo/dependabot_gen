d1eb46165f283f20661a11761f2f1d8ed8712f51
Renan araujo <renanmatheuscorreiaaraujo@gmail.com>
2025-08-21 19:19:20 +0100

    chore: fix tests and formatting

--- test/src/commands/update_command_test.dart
+++ test/src/commands/update_command_test.dart
@@ -50,8 +50,8 @@ void main() {
           currentVersion: any(named: 'currentVersion'),
         ),
       ).thenAnswer((_) async => true);
-      when(() => progress.complete(any())).thenAnswer((_) {
-        final message = _.positionalArguments.elementAt(0) as String?;
+      when(() => progress.complete(any())).thenAnswer((invocation) {
+        final message = invocation.positionalArguments.elementAt(0) as String?;
         if (message != null) progressLogs.add(message);
       });
       when(() => logger.progress(any())).thenReturn(progress);
