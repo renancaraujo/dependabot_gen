cab906de47a30c02f836fad5d07a24a531bd3297
Renan araujo <renanmatheuscorreiaaraujo@gmail.com>
2025-08-21 21:15:31 +0100

    test

--- test/ensure_build_test.dart
+++ test/ensure_build_test.dart
@@ -5,5 +5,17 @@ import 'package:build_verify/build_verify.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('ensure_build', expectBuildClean, tags: ['version-verify']);
+  test(
+      'ensure_build',
+      () => expectBuildClean(
+            customCommand: [
+              'dart',
+              'run',
+              'build_runner',
+              'build',
+              '--delete-conflicting-outputs',
+              '--build-filter="lib/src/*"',
+            ],
+          ),
+      tags: ['version-verify']);
 }
